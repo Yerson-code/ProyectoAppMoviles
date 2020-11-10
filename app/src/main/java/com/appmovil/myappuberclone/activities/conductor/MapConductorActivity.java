96c717cf59dc6268deafb52703565474830ed3c4
YersonBG <yerson.ybg@gmail.com>
2020-11-10 17:32:03 -0500

    Ubicacion del GPS en tiempo real

--- app/src/main/java/com/appmovil/myappuberclone/activities/conductor/MapConductorActivity.java
+++ app/src/main/java/com/appmovil/myappuberclone/activities/conductor/MapConductorActivity.java
@@ -1,7 +1,12 @@
 package com.appmovil.myappuberclone.activities.conductor;
 
 import androidx.annotation.NonNull;
+
+import androidx.appcompat.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,14 +16,57 @@ import android.view.MenuItem;
 import com.appmovil.myappuberclone.R;
 import com.appmovil.myappuberclone.activities.MainActivity;
 import com.appmovil.myappuberclone.datos.AuthProvider;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+
+import com.appmovil.myappuberclone.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.lang.annotation.Target;
 
 public class MapConductorActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
     private AuthProvider authProvider;
+
+
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocation;
+
+    private final static int LOCATION_REQUEST_CODE=1;
+
+    private LocationCallback mLocationCallback=new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult){
+            for (Location location: locationResult.getLocations()){
+                //LOCALIZACION DEL USUARIO EN TIEMPO REAL
+                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                        new CameraPosition.Builder()
+                        .target(new LatLng(location.getLatitude(),location.getLongitude()))
+                        .zoom(15f)
+                        .build()
+                ));
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +83,60 @@ public class MapConductorActivity extends AppCompatActivity implements OnMapRead
     public void onMapReady(GoogleMap googleMap) {
         mMap=googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mLocationRequest=new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(5);
+        mFusedLocation= LocationServices.getFusedLocationProviderClient(this);
+        starLocation();
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==LOCATION_REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                    mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+                }
+            }
+        }
+    }
+
+    private void  starLocation(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+            }else{
+                checkLocationPermission();
+            }
+            }else{
+            mFusedLocation.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper());
+        }
+    }
+    private void checkLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+          if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+              new AlertDialog.Builder(this)
+                      .setTitle("Concede los permisos para continuar")
+              .setMessage("Esta aplicacion requiere los permisos de ubicacion")
+              .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which) {
+                      ActivityCompat.requestPermissions(MapConductorActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST_CODE);
+
+                  }
+              })
+              .create()
+              .show();
+          }else{
+              ActivityCompat.requestPermissions(MapConductorActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST_CODE);
+
+          }
+            }
     }
 
     @Override
