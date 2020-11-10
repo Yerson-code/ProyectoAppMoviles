d57f47199916d80446050e80f7c47f8f3ac6a01e
DerbisHuamanHuancas <hhuancasderbis@crece.uss.edu.pe>
2020-11-10 17:19:10 -0500

    Menus

--- app/src/main/java/com/appmovil/myappuberclone/activities/cliente/MapClienteActivity.java
+++ app/src/main/java/com/appmovil/myappuberclone/activities/cliente/MapClienteActivity.java
@@ -1,19 +1,28 @@
 package com.appmovil.myappuberclone.activities.cliente;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.appmovil.myappuberclone.R;
 import com.appmovil.myappuberclone.activities.MainActivity;
 import com.appmovil.myappuberclone.datos.AuthProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapClienteActivity extends AppCompatActivity {
-        Button  btnCerrarSesion;
+public class MapClienteActivity extends AppCompatActivity  implements OnMapReadyCallback {
+
         AuthProvider authProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +30,36 @@ public class MapClienteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map_cliente);
         getSupportActionBar().setTitle("Mapa Cliente");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        btnCerrarSesion=(Button)findViewById(R.id.logout);
+
+        mMapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+
         authProvider=new AuthProvider();
-        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                authProvider.cerrarSesion();
-                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.conductor_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_lagout){
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    void logout(){
+       authProvider.cerrarSesion();
+        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/appmovil/myappuberclone/activities/conductor/MapConductorActivity.java
+++ app/src/main/java/com/appmovil/myappuberclone/activities/conductor/MapConductorActivity.java
@@ -1,10 +1,16 @@
 package com.appmovil.myappuberclone.activities.conductor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.appmovil.myappuberclone.R;
+import com.appmovil.myappuberclone.activities.MainActivity;
+import com.appmovil.myappuberclone.datos.AuthProvider;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -12,6 +18,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MapConductorActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
+    private AuthProvider authProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +27,8 @@ public class MapConductorActivity extends AppCompatActivity implements OnMapRead
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mMapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
+
+        authProvider=new AuthProvider();
     }
 
     @Override
@@ -27,5 +36,25 @@ public class MapConductorActivity extends AppCompatActivity implements OnMapRead
         mMap=googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.conductor_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_lagout){
+          logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    void logout(){
+        authProvider.cerrarSesion();
+        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
