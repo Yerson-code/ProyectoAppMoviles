1bc1be98f840dfe5e4cfdc4e9e63f4f5b972db7f
YersonBG <yerson.ybg@gmail.com>
2020-11-10 15:27:50 -0500

    Integracion de API Google Maps

--- app/src/main/java/com/appmovil/myappuberclone/activities/conductor/MapConductorActivity.java
+++ app/src/main/java/com/appmovil/myappuberclone/activities/conductor/MapConductorActivity.java
@@ -5,14 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.appmovil.myappuberclone.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapConductorActivity extends AppCompatActivity {
-
+public class MapConductorActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_conductor);
         getSupportActionBar().setTitle("Mapa Conductor");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mMapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
     }
 }
