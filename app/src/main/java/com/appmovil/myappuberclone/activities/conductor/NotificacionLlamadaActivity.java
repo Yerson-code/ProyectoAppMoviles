4c56a9d2d3cce8e35e6a99aa1b997e32aef33012
YersonBG <yerson.ybg@gmail.com>
2020-12-01 20:48:27 -0500

    Diseño de activity de notificacion tipo llamada

---
+++ app/src/main/java/com/appmovil/myappuberclone/activities/conductor/NotificacionLlamadaActivity.java
@@ -0,0 +1,17 @@
+package com.appmovil.myappuberclone.activities.conductor;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.appmovil.myappuberclone.R;
+
+public class NotificacionLlamadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificacion_llamada);
+    }
+}
