8f55e74f76c252faaa1a1dc4edd6d199ec6f89f9
YersonBG <yerson.ybg@gmail.com>
2020-12-09 08:24:20 -0500

    validacion de correo en login

--- app/src/main/java/com/appmovil/myappuberclone/activities/LoginActivity.java
+++ app/src/main/java/com/appmovil/myappuberclone/activities/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,6 +23,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Pattern;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import dmax.dialog.SpotsDialog;
@@ -68,11 +71,21 @@ public class LoginActivity extends AppCompatActivity {
                 PrincipalLogin();
             }
         });
+
+
     }
 
+    private boolean validarEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
     private void PrincipalLogin() {
         String email = mTxtcorreo.getText().toString();
         String password = mTxtpasword.getText().toString();
+        if (!validarEmail(mTxtcorreo.getText().toString())){
+            mTxtcorreo.setError("Email no válido");
+            return;
+        }
 
         if (!email.isEmpty() && !password.isEmpty()){
             if (password.length() >=6){
